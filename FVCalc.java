0abdf8663675b98f7423478c9c49c82cbc754eb9
Hadar Barel <hadarbarel144@gmail.com>
2025-11-05 16:08:55 +0200

    first upload of hw

--- FVCalc.java
+++ FVCalc.java
@@ -8,7 +8,7 @@ public class FVCalc {
 		int n = Integer.parseInt(args[2]);
 		double futureValue = (currentValue * (Math.pow((1+ (rate/100)), n)));
 		System.out.println("After " + n + " years, $" + currentValue + " saved at " 
-		+ rate + " % will yield $" + (int)futureValue);
+		+ rate + "% will yield $" + (int)futureValue);
 		
 	}
 }
